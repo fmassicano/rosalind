5d8a3d1d5edf987cec22b4b60de2fddae7716ddd
Felipe Massicano <massicano@gmail.com>
2019-02-25 18:08:01 -0600

    Adding package utils.

---
+++ utils/utils.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"io"
+	"os"
+)
+
+// Analyze receive a file name and a function which will process the content of that file
+func Analyze(file string, f func(handle io.Reader) error) error {
+	handle, err := os.Open(file)
+
+	if err != nil {
+		return err
+	}
+	defer handle.Close()
+	return f(handle)
+}
