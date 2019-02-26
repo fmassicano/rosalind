a00ec8e606c9db2a5438c1ee67eef9fc4c681ba2
Felipe Massicano <massicano@gmail.com>
2019-02-25 18:55:24 -0600

    Update main

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/fmassicano/rosalind/gccontent"
+	"github.com/fmassicano/rosalind/countingpointmutations"
 	"github.com/fmassicano/rosalind/runner"
 )
 
@@ -12,5 +12,6 @@ func main() {
 	// runner.Run(transcribingdnaintorna.RNA{}, os.Args[1])
 	// runner.Run(complementingdna.DNAcompl{}, os.Args[1])
 	// runner.RunString(rabbitsrecurrencerelations.RabbitRecurrence{}, os.Args[1])
-	runner.RunString(gccontent.GC{}, os.Args[1])
+	// runner.RunString(gccontent.GC{}, os.Args[1])
+	runner.Run(countingpointmutations.CPM{}, os.Args[1])
 }
