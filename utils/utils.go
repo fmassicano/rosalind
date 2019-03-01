abac35342e8fd8d8edd596fcefd9745f0c10a52f
Felipe Massicano <massicano@gmail.com>
2019-02-28 18:13:35 -0600

    Add comments in Analyze func

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,7 @@ import (
 )
 
 // Analyze receive a file name and a function which will process the content of that file
+// Allows line by line reads
 func Analyze(file string, f func(handle io.Reader) error) error {
 	handle, err := os.Open(file)
 
