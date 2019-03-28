1f85502179c8b0f247e987bdf51895d94c76049b
Felipe Massicano <massicano@gmail.com>
2019-03-28 18:09:02 -0500

    Adding functionality

--- LIA/lia.go
+++ LIA/lia.go
@@ -1,1 +1,16 @@
 package LIA
+
+// ∑N_i=n (N i) 0.25^(i) * 0.75^(N−i).
+
+func ncr(N, i int) int {
+
+	return 0
+}
+
+func fact(x int) int {
+	f := 1
+	for i := 1; i <= x; i++ {
+		f = i * f
+	}
+	return f
+}
