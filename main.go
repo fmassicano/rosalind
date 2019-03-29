87b5789ea52a265fae3885ff1ffa6dc1421a54c8
Felipe Massicano <massicano@gmail.com>
2019-03-29 13:22:02 -0500

    Main.go was Updated

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+
+	"github.com/fmassicano/rosalind-branchs/LIA"
+	"github.com/fmassicano/rosalind-branchs/runner"
+)
+
+func main() {
+	// runner.Run(countdnanucleotides.DNArecord{}, os.Args[1])
+	// runner.Run(transcribingdnaintorna.RNA{}, os.Args[1])
+	// runner.Run(complementingdna.DNAcompl{}, os.Args[1])
+	// runner.RunString(rabbitsrecurrencerelations.RabbitRecurrence{}, os.Args[1])
+	// runner.RunString(gccontent.GC{}, os.Args[1])
+	// runner.Run(countingpointmutations.CPM{}, os.Args[1])
+	// runner.RunString(mendelfirstlaw.MendelFirstLaw{}, os.Args[1])
+	// runner.Run(translatingrnaintoprotein.RNAintoProtein{}, os.Args[1])
+	// runner.Run(findingamotifindna.Motif{}, os.Args[1])
+	// runner.RunString(consensusandprofile.Profile{}, os.Args[1])
+	// runner.RunString(mortalfibonaccirabbits.MortalFibonacci{}, os.Args[1])
+	// runner.RunString(overlapgraphs.OverlapGraph{}, os.Args[1])
+	// runner.RunString(calculatingexpectedoffspring.ExpOffSpring{}, os.Args[1])
+	runner.RunString(LIA.LIA{}, os.Args[1])
+
+}
