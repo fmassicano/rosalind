4a8bb7a3db25088e4545179595496819c3318ba8
Felipe Massicano <massicano@gmail.com>
2019-03-29 13:21:25 -0500

    Independent Alleles - Solved / Include a test file

--- LIA/lia_test.go
+++ LIA/lia_test.go
@@ -1,1 +1,27 @@
-package LIA_test
+package LIA
+
+import (
+	"math"
+	"strconv"
+	"testing"
+)
+
+func Test_IndependentsAlleles(t *testing.T) {
+	var eo = LIA{}
+
+	var tests = []struct {
+		s, want string
+	}{
+		{"2 1", "0.684"},
+	}
+
+	for _, c := range tests {
+		got, _ := strconv.ParseFloat(eo.Process(c.s), 64)
+		want, _ := strconv.ParseFloat(c.want, 64)
+
+		if math.Abs(want-got) > 0.001 {
+			t.Errorf("IndependentsAlleles(%v) == %v, want %v", c.s, got, want)
+		}
+	}
+
+}
